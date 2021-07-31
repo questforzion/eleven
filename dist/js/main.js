cd75d355beb2edfac30d5805797e44f8635ec50d
questforzion <questforzion@gmail.com>
2021-07-31 20:27:54 +0200

    newnewnewnew

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,4 +3,18 @@ const hello = (name) => {
   console.log(`hello ${name}`);
 };
 
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav-mbl");
+let show = false;
+
+btn.addEventListener("click", (e) => {
+  if (!show) {
+    nav.classList.add("open");
+    show = true
+  } else {
+    nav.classList.remove("open");
+    show = false
+  }
+});
+
 hello("jo");
